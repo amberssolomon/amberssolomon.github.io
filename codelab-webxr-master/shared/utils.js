e4ccac815174d96078f0a9796bb50feb13f67939
amberssolomon <139155810+amberssolomon@users.noreply.github.com>
2023-07-14 15:28:37 -0400

    Update utils.js

--- codelab-webxr-master/shared/utils.js
+++ codelab-webxr-master/shared/utils.js
@@ -17,7 +17,7 @@ const scene = new THREE.Scene();
 
 const loader = new GLTFLoader()
 loader.load(
-    'apple_vision_home_page_mixed_reality.gltf',
+    "https://immersive-web.github.io/webxr-samples/media/gltf/sunflower/sunflower.gltf",
     function (gltf) {
         // gltf.scene.traverse(function (child) {
         //     if ((child as THREE.Mesh).isMesh) {
