967ae8a112abe7a35ab1a45911d28f3a5447eff0
amberssolomon <139155810+amberssolomon@users.noreply.github.com>
2023-07-16 23:13:45 -0400

    Update utils.js

--- codelab-webxr-master/shared/utils.js
+++ codelab-webxr-master/shared/utils.js
@@ -248,7 +248,7 @@ window.DemoUtils = {
 	object.scene.position.x = 20;
 	//object.scene.position.y = 0;
 	object.scene.position.z = -20;
-	object.scene.scale.multiplyScalar(10);
+	object.scene.scale.multiplyScalar(30);
 	scene2.add(object.scene);
 
 	});
