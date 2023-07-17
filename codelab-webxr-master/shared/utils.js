9e7349c7bcb5ae1a1052dae0b17ee5b3d9cb650c
amberssolomon <139155810+amberssolomon@users.noreply.github.com>
2023-07-16 23:39:38 -0400

    Update utils.js

--- codelab-webxr-master/shared/utils.js
+++ codelab-webxr-master/shared/utils.js
@@ -22,7 +22,7 @@ loader.load("jump_domi1.gltf", (object) => {
 	object.scene.position.y = 0;
 	object.scene.position.z = -20;
 	//object.scene.position.x = 0;
-	object.scene.scale.multiplyScalar(10);
+	object.scene.scale.multiplyScalar(20);
 	scene.add(object.scene);
 
 });
